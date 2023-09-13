ea21e720229751e26963add322f188a403620a41
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-09-13 19:06:22 +1000

    array functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield" ];
+const destructivelyAppendCat = (name) => {
+return cats.push (name);
+};
+const destructivelyPrependCat = (name) => {
+    return cats.unshift(name);
+};
+
+const destructivelyRemoveLastCat = (name) => {
+    return cats.pop(name);
+};
+
+const destructivelyRemoveFirstCat  = (name) => {
+    return cats.shift(name);
+};
+let appendCat = (name) => {
+      let newCats = [...cats, name]
+      return newCats;
+};
+let prependCat =  name => {
+   let newCats = [name, ...cats]
+   return newCats;
+};
+
+const removeLastCat = () => {
+   let lostCats = cats.slice(0,-1)
+   return lostCats;
+};
+
+const removeFirstCat = () => {
+    let lostCat = cats.slice(1)
+    return lostCat;
+}
